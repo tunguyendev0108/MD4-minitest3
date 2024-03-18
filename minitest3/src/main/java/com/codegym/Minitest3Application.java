ba2a5a2b1a199ddc1e002a5f335c0a3be61b34ff
DESKTOP-L93Q7FQ\Admin <nguyenhoangtu1898@gmail.com>
2024-03-18 11:16:36 +0700

    Add MD4-minitest3

---
+++ minitest3/src/main/java/com/codegym/Minitest3Application.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Minitest3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Minitest3Application.class, args);
+    }
+
+}
---
+++ minitest3/src/test/java/com/codegym/Minitest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Minitest3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
